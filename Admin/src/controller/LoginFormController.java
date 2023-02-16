15a6665baf0a6f93cf9bd368dfa143544cbf8725
Gavesh <gaveshthanushika@gmail.com>
2023-02-16 16:50:29 +0530

    Commit one

---
+++ Admin/src/controller/LoginFormController.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Asus
+ */
+public class LoginFormController implements Initializable {
+
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private TextField txtUserName;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private Hyperlink lnkCreateAccount;
+    @FXML
+    private AnchorPane root;
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    } 
+
+    @FXML
+    private void lnkCreateAccountOnClick(ActionEvent event) throws IOException {
+        Parent parent=FXMLLoader.load(getClass().getResource("../view/RegisterForm.fxml"));
+        Scene scene = new Scene(parent);
+        Stage primaryStage=(Stage) root.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+
+    @FXML
+    private void btnLoginOnAction(ActionEvent event) {
+    }
+    
+    
+}
